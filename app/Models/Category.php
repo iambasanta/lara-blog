b8ddaed182911faa1495a03cb951a06ca359164a
iambasanta <sangpangbasanta@gmail.com>
2021-08-06 19:17:53 +0545

    implement eloquent relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['slug', 'name'];
+
     use HasFactory;
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     protected $fillable = ['title', 'slug', 'excerpt', 'body', 'category_id', 'published_at'];
 
     use HasFactory;
+
+    public function category()
+    {
+        return $this->BelongsTo(Category::class);
+    }
 }
