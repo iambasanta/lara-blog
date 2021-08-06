8688c34521804debd6cea111826cc09802c7f680
iambasanta <sangpangbasanta@gmail.com>
2021-08-06 19:35:32 +0545

    implement eloquent relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -15,5 +14,10 @@ class Post extends Model
     public function category()
     {
         return $this->BelongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
